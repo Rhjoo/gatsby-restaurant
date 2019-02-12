841bae8183745c69c2909f0413d72bbdc1e5a14e
ryanhjoo@gmail.com <ryanhjoo@gmail.com>
2019-02-11 23:56:56 -0800

    add images using graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,11 @@ import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/bcg/homeBcg.jpeg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo'
+import Gallery from '../components/HomePageComponents/Gallery'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
-    {/* <h3>Hello from Gatsby</h3>
-    <h3> Lets go for a <FaBeer />? </h3> */}
     <HomeHeader img={img}>
       <Banner title="Eatery" subtitle="55 Main St, Santa Monica, CA">
         <BannerButton style={{ margin: '2rem auto'}}>
@@ -30,6 +22,7 @@ const IndexPage = () => (
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Gallery />
   </Layout>
 )
 
